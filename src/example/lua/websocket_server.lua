5f3c6444ff2ab251d6fb899a4a0999b12bbea3fd
zhaojh329 <jianhuizhao329@gmail.com>
2017-07-03 23:31:14 +0800

    websocket_server.lua: Improve
    Signed-off-by: zhaojh329 <jianhuizhao329@gmail.com>

--- src/example/lua/websocket_server.lua
+++ src/example/lua/websocket_server.lua
@@ -2,13 +2,46 @@
 
 local ev = require("ev")
 local evmg = require("evmongoose")
---local loop = ev.Loop.default
-local loop = ev.Loop.new()
+local loop = ev.Loop.default
+local mgr = evmg.init()
 
-local mgr = evmg.init(loop)
+local page = [[
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>WebSocket Test</title>
+	<script>
+		function connect(){
+			ws = new WebSocket("ws://" + location.host);
+			ws.onopen = function() {
+				console.log("open success");
+				ws.send("Hello Evmg");
+			};
+			
+			ws.onmessage = function(evt) {
+				console.log("read:" + evt.data);
+			};
+			
+			ws.onclose = function () {
+				console.log("server close");
+			};
+		}
+	</script>
+	</head>
+	<body>
+		<button type="button" onclick="connect()">Connect</button>
+	</body>
+</html>
+]]
 
 local function ev_handle(nc, event, msg)
-	if event == evmg.MG_EV_WEBSOCKET_HANDSHAKE_REQUEST then
+	if event == evmg.MG_EV_HTTP_REQUEST then
+		mgr:send_head(nc, 200, #page)
+		mgr:print(nc, page)
+		return true
+		
+	elseif event == evmg.MG_EV_WEBSOCKET_HANDSHAKE_REQUEST then
 		print("method:", msg.method)
 		print("uri:", msg.uri)
 		print("proto:", msg.proto)
@@ -17,7 +50,7 @@ local function ev_handle(nc, event, msg)
 		for k, v in pairs(msg.headers) do
 			print(k, v)
 		end
-
+		
 	elseif event == evmg.MG_EV_WEBSOCKET_FRAME then
 		print(msg.data)
 		mgr:send_websocket_frame(nc, "I is evmg", evmg.WEBSOCKET_OP_TEXT)
