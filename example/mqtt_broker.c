547c3d58d69fcd65372e28da3877b3baf2d7ff95
zhaojh329 <jianhuizhao329@gmail.com>
2017-07-14 13:58:34 +0800

    Add example for mqtt broker
    Signed-off-by: zhaojh329 <jianhuizhao329@gmail.com>

---
+++ example/mqtt_broker.c
@@ -0,0 +1,45 @@
+#include <mongoose.h>
+
+static void signal_cb(struct ev_loop *loop, ev_signal *w, int revents)
+{
+	printf("Got signal: %d\n", w->signum);
+	ev_break(loop, EVBREAK_ALL);
+}
+
+int main(int argc, char *argv[])
+{
+	struct ev_loop *loop = EV_DEFAULT;
+	ev_signal sig_watcher;
+	struct mg_mgr mgr;
+	struct mg_connection *nc;
+	struct mg_mqtt_broker brk;
+	const char *address = "0.0.0.0:1882";
+	ev_signal_init(&sig_watcher, signal_cb, SIGINT);
+	ev_signal_start(loop, &sig_watcher);
+	
+	mg_mgr_init(&mgr, NULL, loop);
+	mg_mqtt_broker_init(&brk, NULL);
+	
+	if ((nc = mg_bind(&mgr, address, mg_mqtt_broker)) == NULL) {
+		fprintf(stderr, "mg_bind(%s) failed\n", address);
+		goto err;
+	}
+	nc->user_data = &brk;
+
+	printf("MQTT broker started on %s\n", address);
+
+	/*
+	* TODO: Add a HTTP status page that shows current sessions
+	* and subscriptions
+	*/
+	
+	ev_run(loop, 0);
+
+err:
+	printf("exit...\n");
+	
+	mg_mgr_free(&mgr);
+
+	return 0;
+}
+
