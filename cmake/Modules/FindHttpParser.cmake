7767620aaba5069d6935f66db9f6d110c7c59cdd
zhaojh329 <jianhuizhao329@gmail.com>
2017-07-21 23:53:08 +0800

    Fix FindHttpParser.cmake

--- cmake/Modules/FindHttpParser.cmake
+++ cmake/Modules/FindHttpParser.cmake
@@ -1,20 +1,20 @@
 # - Try to find http-parser
 # Once done this will define
-#  HTTP_PARSER_FOUND        - System has http-parser
-#  HTTP_PARSER_INCLUDE_DIRS - The http-parser include directories
-#  HTTP_PARSER_LIBRARIES    - The libraries needed to use http-parser
+#  HTTPPARSER_FOUND        - System has http-parser
+#  HTTPPARSER_INCLUDE_DIRS - The http-parser include directories
+#  HTTPPARSER_LIBRARIES    - The libraries needed to use http-parser
 
-find_path(HTTP_PARSER_INCLUDE_DIR
+find_path(HTTPPARSER_INCLUDE_DIR
   NAMES http_parser.h
 )
-find_library(HTTP_PARSER_LIBRARY
+find_library(HTTPPARSER_LIBRARY
   NAMES http_parser
 )
 
-if(HTTP_PARSER_INCLUDE_DIR)
-  file(STRINGS "${HTTP_PARSER_INCLUDE_DIR}/http_parser.h"
+if(HTTPPARSER_INCLUDE_DIR)
+  file(STRINGS "${HTTPPARSER_INCLUDE_DIR}/http_parser.h"
     HTTP_PARSER_VERSION_MAJOR REGEX "^#define[ \t]+HTTP_PARSER_VERSION_MAJOR[ \t]+[0-9]+")
-  file(STRINGS "${HTTP_PARSER_INCLUDE_DIR}/http_parser.h"
+  file(STRINGS "${HTTPPARSER_INCLUDE_DIR}/http_parser.h"
     HTTP_PARSER_VERSION_MINOR REGEX "^#define[ \t]+HTTP_PARSER_VERSION_MINOR[ \t]+[0-9]+")
   string(REGEX REPLACE "[^0-9]+" "" HTTP_PARSER_VERSION_MAJOR "${HTTP_PARSER_VERSION_MAJOR}")
   string(REGEX REPLACE "[^0-9]+" "" HTTP_PARSER_VERSION_MINOR "${HTTP_PARSER_VERSION_MINOR}")
@@ -24,15 +24,15 @@ if(HTTP_PARSER_INCLUDE_DIR)
 endif()
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set HTTP_PARSER_FOUND to TRUE
+# handle the QUIETLY and REQUIRED arguments and set HTTPPARSER_FOUND to TRUE
 # if all listed variables are TRUE and the requested version matches.
 find_package_handle_standard_args(HttpParser REQUIRED_VARS
-                                  HTTP_PARSER_LIBRARY HTTP_PARSER_INCLUDE_DIR
+                                  HTTPPARSER_LIBRARY HTTPPARSER_INCLUDE_DIR
                                   VERSION_VAR HTTP_PARSER_VERSION)
 
-if(HTTP_PARSER_FOUND)
-  set(HTTP_PARSER_LIBRARIES     ${HTTP_PARSER_LIBRARY})
-  set(HTTP_PARSER_INCLUDE_DIRS  ${HTTP_PARSER_INCLUDE_DIR})
+if(HTTPPARSER_FOUND)
+  set(HTTPPARSER_LIBRARIES     ${HTTPPARSER_LIBRARY})
+  set(HTTPPARSER_INCLUDE_DIRS  ${HTTPPARSER_INCLUDE_DIR})
 endif()
 
-mark_as_advanced(HTTP_PARSER_INCLUDE_DIR HTTP_PARSER_LIBRARY)
+mark_as_advanced(HTTPPARSER_INCLUDE_DIR HTTPPARSER_LIBRARY)
